5bec84e7dcb236058ec0474dd338d661f7fd540f
nazaninnasab <dranoer@gmail.com>
2023-06-12 03:18:29 +0200

    Add compose dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,10 +40,12 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
+
+    //Compose
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material)
+    //Compose Utils
+    implementation(libs.activity.compose)
 }
